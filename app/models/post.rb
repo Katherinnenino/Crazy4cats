bb927899544c478f9786391f6b1a56f677ac08e8
Katherinne Niño <kattyvaleninovillesca@gmail.com>
2023-07-17 04:20:09 +0000

    editando vistas y agregando reacciones

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :reactions, dependent: :destroy
-  
+  accepts_nested_attributes_for :comments, reject_if: :no_name_category
   Kinds = %w[like dislike].freeze
 end
